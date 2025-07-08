576025e876fc548460b1b5c7ed49debd98a9bf9b
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-07-07 19:59:19 -1000

    revamp from start to figure out how to write the objects

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
-// Global Constants
-const ROWS = 3;
-const COLUMNS = 3;
-
-
-// Functions and Objects
-
-    // create gameboard as an IIFE
-const gameboard = (function () {
-    let boardArr = [];
-
-    const buildBoard = (rows, cols) => {
-        boardArr = [];  // reset array
-        for (let i = 0; i < rows; i++) {
-            boardArr[i] = [];   // inner arr for each row
-            for (let j = 0; j < cols; j++) {
-                boardArr[i][j] = "empty";
-            }
-        }
-        return boardArr;
+function createPlayer (stamp) {
+    getName: function () =>{
+        prompt("What is your name?")
     }
-    return { boardArr, buildBoard };
-})();
 
-    // create player factory function
-function createPlayer (name, symbol) {
-    return { name, symbol };
 }
 
-// Execute
-    // create a board to play on
-let ticTacToeBoard = gameboard.buildBoard(ROWS, COLUMNS);
 
-const player1 = createPlayer('Lephos', 'X');
-const player2 = createPlayer('Melissa', 'O');
+// Gameboard represents the state of the board
+// Each square holds a Cell which starts as "empty" but is
+// replaced with "X" or "O"
+
+/* function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    let board = [];
+
+    // 2d array that represents the state of the game board
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+
+        
+    }
+
+    // get board for UI to render it
+        const getBoard = () => board;
+
+    return {
+        getBoard
+    };
+}
+
+function Cell() {
+    let value = 'empty';
+
+    // a player stamps 'X' or 'O' in an empty cell
+    const stampCell = (player) => { 
+        value = player.symbol;
+    }
+
+    const getStamp = () => value;
+
+    return {
+        stampCell,
+        getStamp
+    };
+}
+
+var game = {
+    turn: 1,    // turns count up each time a player makes a choice; 9 turns total
+    // draw board
+    // get player names from console
+    // determine first player ('X')
+    // ask player1 for coordinates
+    // set cell from "empty" to "X" or "O"
+    // check for winner if round > 5
+    // ask player2 for coordinates
+    // set cell from "empty" to "X" or "O"
+    // check for winner if round > 5
+};
+
+var player = {
+    // player name
+}
+
